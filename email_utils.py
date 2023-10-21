bff6ffe68d2af838d9d6b5bfbf576667af5386a2
Raspberry Pi 4B <raspberrytest12345@gmail.com>
2023-10-21 17:55:45 -0300

    adiciona mais mensagens de email para o aluno

--- email_utils.py
+++ email_utils.py
@@ -10,11 +10,26 @@ USERNAME = os.environ['user'] # email do raspberry para login
 PASS = os.environ['pass'] # senha do email do raspberry para login
 REMETENTE = os.environ['email'] # email do remetente (quem vai enviar a mensagem)
 
+# Envia um email para o aluno contabilizando que teve falta na aula e foi contabilizado em nosso sistema
+def envia_email_acusando_falta(aluno, ID, destinatario):
+    assunto_email = "Atenção: Você recebeu falta!"
+    mensagem_email = f"Caro(a) {aluno}, de RA: {ID}:\n\nInformamos que sua presença em sala de aula não foi contabilizada e portanto foi atribuída falta!"
+
+    enviar_email(destinatario, assunto_email, mensagem_email)
+
 
 # Envia um email de alerta para o aluno caso não seja contabilizada sua presença na aula
 def envia_email_alerta(aluno, ID, destinatario):
     assunto_email = "Alerta: Sua presença não foi contabilizada até o momento!" # assunto do email a ser enviado
-    mensagem_email = f"Atenção {aluno} - RA: {ID}:\nInformamos que até o presente momento, não detectamos sua presença em sala de aula. Caso se mantenha desta forma, será atribuida falta para este dia letivo. Se você está presente na sala, contate o professor responsável." # mensagem do email a ser enviado
+    mensagem_email = f"Caro(a) {aluno}, de RA: {ID}:\n\nInformamos que até o presente momento, não detectamos sua presença em sala de aula. Caso se mantenha desta forma, será atribuida falta para este dia letivo. Se você está presente em sala, contate o professor responsável." # mensagem do email a ser enviado
+
+    enviar_email(destinatario, assunto_email, mensagem_email)
+
+
+# Envia um email de confirmação para o aluno, informando que ele está presente em sala
+def envia_email_confirmando_presenca(aluno, ID, destinatario):
+    assunto_email = "Confirmação: Sua presença foi confirmada!"
+    mensagem_email = f"Caro(a) {aluno}, de RA: {ID}:\n\nInformamos que sua presença em sala de aula foi confirmada e foi contabilizada em nosso sistema!"
 
     enviar_email(destinatario, assunto_email, mensagem_email)
 
